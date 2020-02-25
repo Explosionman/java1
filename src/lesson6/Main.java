7fdd29bdd70aa67c1d24eea17c07938fe19a983a
Alexandr Rybinskov <rybinskov@gmail.com>
2020-02-25 23:28:54 +0300

    Trying to made homework №6

--- src/lesson6/Main.java
+++ src/lesson6/Main.java
@@ -2,6 +2,28 @@ package lesson6;
 
 public class Main {
     public static void main(String[] args) {
-       
+        //Создаём экземпляры классов Cat и Dog
+        Cat catBoris = new Cat("Борис");
+        Cat catMurzik = new Cat("Мурзик");
+        Dog dogTaddy = new Dog("Тэдди");
+        Dog dogDobi = new Dog("Доби");
+        Dog dogBobby = new Dog("Бобби");
+
+        //Применяем метода к созданным экземплярам класса Cat
+        catBoris.run(220);
+        catBoris.swim(5);
+        catMurzik.run(20);
+        catMurzik.swim(0);
+
+        //Применяем метода к созданным экземплярам класса Dog
+        dogTaddy.run(400);
+        dogTaddy.swim(5);
+        dogDobi.run(400);
+        dogDobi.swim(15);
+        dogBobby.run(1000);
+        dogBobby.swim(6);
+
+        System.out.println("Итого создано животных - " + dogTaddy.getAnimalCounter() + ": из них котов - "
+                + catBoris.getCatCounter() + ", собак - " + dogTaddy.getDogCounter());
     }
 }
